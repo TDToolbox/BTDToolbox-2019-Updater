dea9a97518e45e69f41a1c385776b0e41c3287eb
Gurrenm3 <ThomasPMar@hotmail.com>
2020-02-02 19:45:45 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BTDToolbox_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Console = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(27, 186);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(527, 37);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Console
+            // 
+            this.Console.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.Console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Console.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Console.ForeColor = System.Drawing.Color.White;
+            this.Console.Location = new System.Drawing.Point(27, 98);
+            this.Console.Name = "Console";
+            this.Console.ReadOnly = true;
+            this.Console.Size = new System.Drawing.Size(527, 82);
+            this.Console.TabIndex = 2;
+            this.Console.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BTDToolbox_Updater.Properties.Resources.toolboxUpdater_img_1_bg;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(136, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 53);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(48, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Console:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(589, 235);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Console);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox Console;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using Ionic.Zip;
+using Ionic.Zlib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+//using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTDToolbox_Updater
+{
+    public partial class Form1 : Form
+    {
+
+        public string lastMessage;
+        Thread bgThread;
+        WebClient client;
+        int totalFiles;
+        int filesTransfered;
+        string toolboxRelease;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            client = new WebClient();
+            
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            printToConsole("Program Initialized...");
+            printToConsole("Getting download link for latest version...");
+            string versionText = client.DownloadString("https://raw.githubusercontent.com/TDToolbox/BTDToolbox-2019_LiveFIles/master/Version");
+            string[] split = versionText.Split('\n');
+            toolboxRelease = split[0].Replace("toolbox2019: ", "");
+            printToConsole("Download link aquired!");
+            start();
+        }
+        private void start()
+        {
+            printToConsole("Checking if Toolbox is running..");
+            if (isToolboxRunning() == false)
+            {
+                printToConsole("Toolbox is not running...");
+                BeginUpdate();
+            }
+            else
+            {
+                try
+                {
+                    foreach (Process proc in Process.GetProcessesByName("BTDToolbox"))
+                    {
+                        printToConsole("Toolbox is running, terminating toolbox...");
+                        proc.Kill();
+                        printToConsole("Toolbox terminated...");
+                    }
+                    BeginUpdate();
+                }
+                catch (Exception ex)
+                {
+                    printToConsole(ex.Message);
+                }
+            }
+        }
+        private void BeginUpdate()
+        {
+            printToConsole("Beginning update...");
+            printToConsole("The update will delete the old toolbox files...\n\nDo you want to delete all of the projects as well?");
+            DialogResult result = MessageBox.Show("The update will delete the old toolbox files...\n\nDo you want to delete all of the projects as well?", "Delete project files?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+                DeleteDirectory(Environment.CurrentDirectory, true);
+            else
+                DeleteDirectory(Environment.CurrentDirectory, false);
+
+            bgThread = new Thread(DownloadUpdate);
+            bgThread.Start();
+        }
+        private void DownloadUpdate()
+        {
+            printToConsole("Downloading update...");
+            client.DownloadFile(toolboxRelease, "Update"); //, Environment.CurrentDirectory);//
+            printToConsole("Update successfully downloaded!");
+            File.Move("Update", "BTDToolbox.zip");
+            extract();
+        }
+        private void extract()
+        {
+            printToConsole("Extracting update files....");
+            string zipPath = Environment.CurrentDirectory + "\\BTDToolbox.zip";
+            string extractedFilePath = Environment.CurrentDirectory;
+            ZipFile archive = new ZipFile(zipPath);
+            
+            totalFiles = archive.Count();
+            filesTransfered = 0;
+            archive.ExtractProgress += ZipExtractProgress;
+
+            foreach (ZipEntry e in archive)
+            {
+                e.Extract(extractedFilePath, ExtractExistingFileAction.DoNotOverwrite);
+            }
+            archive.Dispose();
+            printToConsole("Update files successfully extracted!!!\nDeleting installation files...");
+            File.Delete(zipPath);
+            printToConsole("Installation files deleted. Restarting Toolbox...");
+            Process.Start(Environment.CurrentDirectory + "\\BTDToolbox.exe");
+            Application.Exit();
+        }
+        private void ZipExtractProgress(object sender, ExtractProgressEventArgs e)
+        {
+            
+            if (e.EventType != ZipProgressEventType.Extracting_BeforeExtractEntry)
+                return;
+            try
+            {
+                progressBar1.Invoke(new Action(() => progressBar1.Value = 100 * filesTransfered / totalFiles));
+            }
+            catch (Exception ex)
+            {
+                printToConsole(ex.Message);
+            }
+            filesTransfered++;
+        }
+        private bool isToolboxRunning()
+        {
+            if (Process.GetProcessesByName("BTDToolbox").Length > 0)
+                return true;
+            else
+                return false;
+        }
+        private void printToConsole(string message)
+        {
+            if (message != lastMessage)
+            {
+                Invoke((MethodInvoker)delegate {
+                    Console.AppendText(">> " + message + "\r\n");
+                    lastMessage = message;
+                });
+            }
+        }
+        private void DeleteDirectory(string path, bool deleteProjs)
+        {
+            if (Directory.Exists(path))
+            {
+                printToConsole("Deleting directory..");
+                var directory = new DirectoryInfo(path) { Attributes = FileAttributes.Normal };
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    if ((deleteProjs == true))
+                    {
+                        if (!file.ToString().Contains("BTDToolbox_Updater") && (!file.ToString().Contains(".dll")) && (!file.ToString().Contains(".json")))
+                            file.Delete();
+                    }
+                    else
+                    {
+                        if (!file.ToString().Contains("BTDToolbox_Updater") && (!file.ToString().Contains("proj")) && (!file.ToString().Contains(".dll")) && (!file.ToString().Contains(".json")))
+                            file.Delete();
+                    }
+                    
+
+                }
+                foreach (DirectoryInfo dir in directory.GetDirectories())
+                {
+                    
+                    if ((deleteProjs == true))
+                    {
+                        if (dir.ToString() != Environment.CurrentDirectory)
+                            dir.Delete(true);
+                    }
+                    else
+                    {
+                        if ((dir.ToString() != Environment.CurrentDirectory) && (!dir.ToString().Contains("proj")))
+                            dir.Delete(true);
+                    }
+                }
+                printToConsole("Directory deleted.");
+            }
+            else
+            {
+                printToConsole("Directory not found. Unable to delete directory at:\r\n" + path);
+            }
+        }
+        private bool checkForFiles(string path)
+        {
+            bool filesExist = false;
+            printToConsole("Confirming if directory is empty..");
+            var directory = new DirectoryInfo(path) { Attributes = FileAttributes.Normal };
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (!file.ToString().Contains("BTDToolbox_Updater") && (!file.ToString().Contains("proj")) && (!file.ToString().Contains(".dll")) && (!file.ToString().Contains(".json")))
+                {
+                    filesExist = true;
+                    break;
+                }
+            }
+            return filesExist;
+        }
+
+    }
+}
